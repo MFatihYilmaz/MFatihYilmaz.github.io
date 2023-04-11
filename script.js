fc4b8971a430419d9a8365c90e676a79f8cdddd0
MFatihYilmaz <mfth78@hotmail.com>
2023-04-11 17:16:26 +0300

    animation added

--- script.js
+++ script.js
@@ -13,6 +13,24 @@ document.querySelector(".btn").addEventListener("click",()=>{
     }
    
 })
+function reveal() {
+    var reveals = document.querySelectorAll(".effect-3");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 100;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].children[0].classList.add("animate")
+        
+      } else {
+        reveals[i].children[0].classList.remove("animate")
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
 
 document.querySelector("#msc").addEventListener("click",()=>{
     aud.load()
